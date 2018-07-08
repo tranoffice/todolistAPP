63693bd97dafe6c0a7fd66fdc7274555ef3b4721
tranoffice <bertrand.tran.office@gmail.com>
2018-07-08 19:20:07 +0200

    Update Trello.js

--- src/Trello.js
+++ src/Trello.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 
 export default class TrelloComponent extends Component {
+    styleStatusRed = {fontSize : '0.7em', color: 'red'};
+    styleStatusBlue = {fontSize : '0.7em', color: 'blue'};
+    styleStatusGreen = {fontSize : '0.7em', color: 'green'};
+
     constructor(props) {
         super(props);
         this.state = {
-            styleStatus: {fontSize : '0.7em', color: 'red'},
+            styleStatus: this.styleStatusRed,
             styleAction: { display: 'none' },
             edit: false,
             task: this.props.item,
         }
     }
 
+    getStyleStatus ( status ) {
+        let ret = this.styleStatusRed
+        if ( status === 'In progress' ) {
+            ret = this.styleStatusBlue;
+        }
+        if ( status === 'Completed' ) {
+            ret = this.styleStatusGreen;
+        }
+        return(ret);
+    }
+
     render() {
         return (
             <div className="trello-container">
                 <div className="trello-header">
-                    <span style={this.state.styleStatus}>{this.state.task.status}</span>
+                    <span style={this.getStyleStatus(this.state.task.status)}>{this.state.task.status}</span>
                     <span className="fill-remaining-space"></span>
                     <button style={ {fontSize : '0.7em' }} onClick={this.onEditOrUpdate}>{this.state.edit ? 'Enregistrer' : 'Edit'}</button>
                 </div>
@@ -52,18 +67,19 @@ export default class TrelloComponent extends Component {
     }
     onChangeStatus1 = (event) => {
         this.setState({ task: { status: 'Todo', task: this.state.task.task, id: this.state.task.id }, 
-                        styleStatus: {fontSize : '0.7em', color: 'red'}
+                        styleStatus: this.styleStatusRed
         });
     }
     onChangeStatus2 = (event) => {
         this.setState({ task: { status: 'In progress', task: this.state.task.task, id: this.state.task.id }, 
-                        styleStatus: {fontSize : '0.7em', color: 'blue'}
+                        styleStatus: this.styleStatusBlue
         });
     }
     onChangeStatus3 = (event) => {
         this.setState({ task: { status: 'Completed', task: this.state.task.task, id: this.state.task.id  }, 
-                        styleStatus: {fontSize : '0.7em', color: 'green'}
+                        styleStatus: this.styleStatusGreen
         });
     }
 
 };
+
